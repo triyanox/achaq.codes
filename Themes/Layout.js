c862a913c6b7a6f57dae503af2b641657193fbcd
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-02-03 00:30:52 +0100

    PWA Support

--- Themes/Layout.js
+++ Themes/Layout.js
@@ -33,26 +33,6 @@ const Layout = (props) => {
           key="ogdesc"
         />
         <title>{props.pageTitle}</title>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="theme-color" content="#000000" />
       </Head>
       <div className=" scroll-smooth py-12">
         <ThemeProvider>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,

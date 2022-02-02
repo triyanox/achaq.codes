977e4a6860623b646e8363bd35dcf32949af2a5f
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-02-03 00:06:37 +0100

    Manifest Added

--- Themes/Layout.js
+++ Themes/Layout.js
@@ -50,7 +50,7 @@ const Layout = (props) => {
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="theme-color" content="#000000" />
       </Head>

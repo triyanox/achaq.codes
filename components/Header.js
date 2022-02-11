b30032c98679a2e12ee912039bab3685dec7fcf9
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-02-11 17:18:41 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ function Header() {
     <section className="p-2 mt-0 fixed h-12 w-full z-20 top-0  ">
       <div>
         <nav className="bg-opacity-90 dark:bg-opacity-90 backdrop-blur-xl p-3 flex items-center flex-row px-8 mx-2 lg:mx-12 rounded-3xl h-12">
-          <div className=" w-full bg-transparent dark:bg-transparent lg:inline-flex backdrop-blur-xl hidden lg:visible">
+          <div className=" w-full lg:inline-flex backdrop-blur-xl hidden lg:visible">
             <Link href="/">
               <a className="lg:inline-flex lg:w-auto w-full px-3 pb-8 pt-12 lg:pb-0 lg:pt-0 text-gray-800 dark:text-gray-300  items-centertext-opacity-80justify-center active:scale-90 duration-500 ">
                 Home

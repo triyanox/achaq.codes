66487cc745b0c709ee3dd5b904a99d2c64a1608f
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-02-12 22:56:47 +0100

    Update About.js

--- components/About.js
+++ components/About.js
@@ -27,8 +27,8 @@ const About = () => {
           <div className="absolute -bottom-8 left-20 w-72 h-72 bg-purple-800  rounded-full mix-blend-multiply  filter blur-xl opacity-70 animate-pulse"></div>
           <Image
             src={dev}
-            width={422,4}
-            height={364,8}
+            width={528}
+            height={456}
             alt="Developer"
           />
         </div>

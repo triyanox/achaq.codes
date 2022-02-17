9b33e7586bff3ce9bd0fe040d47a4316417e1895
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-02-17 21:16:26 +0100

    Update About.js

--- components/About.js
+++ components/About.js
@@ -1,4 +1,4 @@
-import dev from "../assets/dev.png";
+import dev from "../assets/dev.svg";
 import Image from "next/image";
 const About = () => {
   return (
@@ -36,7 +36,7 @@ const About = () => {
           <div className="absolute top-0 -left-4 w-72 h-72 bg-cyan-500 rounded-full mix-blend-multiply  filter blur-xl opacity-70 animate-pulse"></div>
           <div className="absolute top-0 -right-4 w-72 h-72 bg-cyan-500 rounded-full mix-blend-multiply  filter blur-xl opacity-70 animate-pulse"></div>
           <div className="absolute -bottom-8 left-20 w-72 h-72 bg-purple-800  rounded-full mix-blend-multiply  filter blur-xl opacity-70 animate-pulse"></div>
-          <Image src={dev} width={800} height={690.41} alt="Developer" />
+          <Image src={dev} width={528} height={456} alt="Developer" />
         </div>
       </div>
     </section>
